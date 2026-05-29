d07857635b172186194c93b3cde0c3e1b9ea31bf
yogeshbr393-hue <yogeshbr393@gmail.com>
2026-05-29 12:32:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+# Login Page
+@app.route('/', methods=['GET', 'POST'])
 def login():
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Simple Login Check
+        if username == "admin" and password == "admin":
+            return redirect(url_for('dashboard'))
+
     return render_template('login.html')
 
+
+# Dashboard Page
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
+
+# Admin Page
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
 
+
+# About Page
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+# Attack Logs Page
 @app.route('/attack_logs')
 def attack_logs():
     return render_template('attack_logs.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
