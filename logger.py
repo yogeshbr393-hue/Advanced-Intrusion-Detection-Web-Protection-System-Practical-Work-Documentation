390ecdbe5cc6549b75e2472eded424900eca0cc5
yogeshbr393-hue <yogeshbr393@gmail.com>
2026-05-29 10:35:15 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,25 +1,4 @@
-from datetime import datetime
-from database import insert_attack
+from database import insert_log
 
-# Log file path
-LOG_FILE = "logs/attacks.log"
-
-# Logging function
-def log_attack(ip, attack_type, payload):
-
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    log_entry = (
-        f"{timestamp} | "
-        f"IP: {ip} | "
-        f"Attack: {attack_type} | "
-        f"Payload: {payload}\n"
-    )
-
-    # Save to log file
-    with open(LOG_FILE, "a") as file:
-
-        file.write(log_entry)
-
-    # Save to SQLite database
-    insert_attack(attack_type, payload, ip)
+def log_attack(ip, threat):
+    insert_log(ip, threat)
